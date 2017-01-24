d8887b37cb6d50eff6bfd2c7d4a9a43a248caf81
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-01-24 14:33:34 -0800

    latest update 2:33

--- src/test/java/com/xyzcorp/tdd/ParserTest.java
+++ src/test/java/com/xyzcorp/tdd/ParserTest.java
@@ -2,11 +2,18 @@ package com.xyzcorp.tdd;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class ParserTest {
 
@@ -26,5 +33,49 @@ public class ParserTest {
 		Parser parser = new Parser();
 		Stream<Checkout> checkouts = parser.parseStream(data);
 		assertThat(checkouts).hasSize(2);
+		
+		
 	}
+	
+	@Test
+	public void testIntegrationWithClasspath() {
+		InputStream is = getClass().getResourceAsStream("/library.txt");
+		InputStreamReader reader = new InputStreamReader(is);
+		BufferedReader buff = new BufferedReader(reader);
+		Parser parser = new Parser();
+		Stream<Checkout> checkouts = parser.parseStream(buff.lines());
+		assertThat(checkouts).hasSize(16);
+	}
+	
+	@Test
+	public void testWithURLWebService() throws IOException {
+		URL url = 
+		new URL("https://raw.githubusercontent.com/dhinojosa/tdd20170123/master/src/main/resources/library.txt");
+		InputStream is = url.openConnection().getInputStream();
+		InputStreamReader reader = new InputStreamReader(is);
+		BufferedReader buff = new BufferedReader(reader);
+		Parser parser = new Parser();
+		Stream<Checkout> checkouts = parser.parseStream(buff.lines());
+		assertThat(checkouts).hasSize(16);
+	}
+	
+	@Test
+	public void testMulipleLineParseWithMockBufferedReader() throws IOException {
+        BufferedReader reader = Mockito.mock(BufferedReader.class);
+		Mockito.when(reader.readLine())
+		  .thenReturn("Janelle Klein~On Intelligence~2014-07-21",
+				      "Jana Nguyen~To Kill a Mockingbird~2016-01-02");
+		          
+		//Parser parser = new Parser();
+		//Stream<Checkout> checkouts = parser.parseStream(data);
+		//assertThat(checkouts).hasSize(2);
+	}
+
+	
+	//TODO: Sink your code! This is not done yet.
+	// 1. Blank Line
+	// 2. Too many/too few fields
+	// 3. Date in the future
+	
+	
 }
